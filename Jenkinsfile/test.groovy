e49cb14c26c3c19429f35e65e8c2afc1bf778ddf
alienquacker <yuitka44@gmail.com>
2020-06-21 22:07:24 +0500

    Create test.groovy

---
+++ Jenkinsfile/test.groovy
@@ -0,0 +1,36 @@
+    
+pipeline {
+    agent{node('master')}
+    stages {
+        stage('Dowload project') {
+            steps {
+                script {
+                    cleanWs()
+                    //git(branch: 'master', credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git')
+                }
+                script {
+                    echo 'Start download project'
+                    checkout([$class                           : 'GitSCM',
+                              branches                         : [[name: '*/master']],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions                       : [[$class           : 'RelativeTargetDirectory',
+                                                                   relativeTargetDir: 'auto']],
+                              submoduleCfg                     : [],
+                              userRemoteConfigs                : [[credentialsId: 'IlyaBurakGit', url: 'https://github.com/ilyaburak/study_jenkins.git']]])
+                }
+            }
+        }
+        stage ('Create docker image'){
+            steps{
+                script{
+                    sh "docker build ${WORKSPACE}/auto -t webapp"
+                    sh "docker run -d webapp"
+                    sh "docker exec -it webapp "df -h > ~/proc""
+                }
+            }
+        }
+        
+    }
+
+    
+}
